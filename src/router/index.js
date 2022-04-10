956ad8c5e4e9ab6079647d0fe43801daddf90e7a
Shion Ichikawa <shion1305@gmail.com>
2022-04-10 19:54:57 +0900

    [ADD] corresponding pages

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/EventList',
+    name: 'event_list',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/EventDetail',
+    name: 'event_detail',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
 ]
 
 const router = createRouter({
