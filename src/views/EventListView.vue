0cde3a94f2810bd4bf041e098c583a35d1521fed
Shion Ichikawa <shion1305@gmail.com>
2022-04-23 02:15:35 +0900

    [IMPLEMENT] EventListView.vue as first version

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,13 +1,52 @@
 <template>
-  <h1>event page</h1>
+  <div class="content-frame">
+    <div>
+      <!--Control area-->
+    </div>
+    <div>
+      <div class="events_block">
+        <!--        <router-link v-for="event in eventData" :key="event.key" :to="{name:'event_detail,',params:{id:event.id}}" }>-->
+        <div v-for="event in eventData" :key="event.key">
+          <EventTile :event="event"></EventTile>
+          <!--        </router-link>-->
+        </div>
+      </div>
+      <!--      </router-link>-->
+    </div>
+  </div>
 </template>
 
 <script>
+import EventTile from "@/components/EventTile";
+import data from "@/assets/data.json";
+
 export default {
-  name: "EventListView"
+  name: "EventListView",
+  components: {EventTile},
+  computed: {
+    eventData() {
+      console.log(data);
+      return data;
+    }
+  }
 }
 </script>
-
 <style scoped>
+.events_block {
+  box-sizing: border-box;
+  justify-content: center;
+  gap: 0.5rem;
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 70rem;
+  width: 100%;
+}
 
+.content-frame {
+  display: flex;
+  flex-direction: column;
+  width: min(100% - 2rem, 70rem);
+  margin-inline: auto;
+}
 </style>
