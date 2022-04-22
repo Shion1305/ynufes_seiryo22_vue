480be3bb1958fe3dcecf5e4d4980d4989c959721
Shion Ichikawa <shion1305@gmail.com>
2022-04-23 02:17:31 +0900

    [UPDATE] index.js(some are replaced with temporary page.)

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/EventList',
-    name: 'event_list',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/EventDetail',
-    name: 'event_detail',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/UnderConstruction')
+    },
+    {
+        path: '/events',
+        name: 'event_list',
+        component: () => import(/* webpackChunkName: "about" */ '../views/EventListView')
+    },
+    {
+        path: '/events/:id',
+        name: 'event_detail',
+        component: () => import(/* webpackChunkName: "about" */ '../views/EventDetailView.vue'),
+        props: (route) => ({...route.params, id: parseInt(route.params.id)})
+    },
+    {
+        path: '/pamphlet',
+        name: 'pamphlet',
+        component: () => import(/* webpackChunkName: "about" */ '../views/UnderConstruction.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
