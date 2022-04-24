861499d13b625ee3a6ebb0be898765059c9dd40c
Shion Ichikawa <shion1305@gmail.com>
2022-04-25 05:07:02 +0900

    [IMPLEMENT] UnderConstruction.vue

--- src/views/UnderConstruction.vue
+++ src/views/UnderConstruction.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>現在このページは作成中です。完成までしばらくお待ちください。</div>
+  <div class="content-frame fadeUp" style="animation-delay: 0.2s">
+    <div class="fadeUp" style="animation-delay: 0.4s">
+      <img src="@/assets/kamome_sempai/writing.png" alt="かもめ先輩、勉強中!"/>
+      <div style="font-size: 3rem">404</div>
+      <div id="not-found">このページは作成中です!<br>公開までしばらくお待ちください</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "UnderConstruction"
+  name: "NotFoundView"
 }
 </script>
 
 <style scoped>
+#not-found{
+  font-size: 1.2rem;
+}
+.content-frame {
+  white-space: nowrap;
+  width: min(100% - 2rem, 60rem);
+  margin-inline: auto;
+  display: flex;
+  position: relative;
+  text-align: center;
+  background-size: cover;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 2rem;
+  background-image: url("/public/image2.webp");
+  margin-bottom: 1rem;
+}
 
+.content-frame img {
+  background-size: contain;
+  width: 100%;
+  max-width: 300px;
+}
+
+.content-frame > div {
+  width: fit-content;
+  margin: 20px;
+  background: #cd4fffcc;
+  color: white;
+  padding: 1rem 1.5rem;
+  border-radius: 3rem;
+}
+@media screen and (max-width: 450px) {
+  #not-found{
+    font-size: 18px;
+  }
+}
 </style>
