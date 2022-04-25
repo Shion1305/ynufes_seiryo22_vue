5825d1f662ee9f07de29df4177b6ada0f853768a
Shion Ichikawa <shion1305@gmail.com>
2022-04-26 03:22:05 +0900

    [CHANGE] change sponsors, add acknowledgments, some change in design

--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -1,31 +1,69 @@
 <template>
   <div class="content-frame fadeUp">
-    <img src="@/assets/imgs/image3.webp"/>
-    <h1 class="title">ご協賛企業一覧</h1>
+    <img src="@/assets/imgs/image3.webp" alt="背景画像"/>
+    <div id="page_top">
+      <h1 class="title">ご協賛企業一覧</h1>
+      <div><span>「22清陵祭」を開催するにあたり、</span>多くの企業の皆様より多大なご協力を賜りました。この場をお借りいたしまして、心より感謝申し上げます。ご協賛いただきました企業を以下に掲載させていただきます。</div>
+    </div>
     <div class="section_holder">
       <section class="section fadeLeft delay04" style="width:min(100% - 1rem, 27rem)">
         <div class="section_head"/>
         <div class="section_content">
-          <h2>パンフレット協賛</h2>
+          <h2>物品協賛</h2>
           <hr>
           <ul>
-            <li>株式会社有隣堂</li>
-            <li>横浜弁当</li>
-            <li>寺田倉庫株式会社</li>
-            <li>ユニゾン英語学院</li>
-            <li>杉本歯科医院</li>
-            <li>ねぎしフードサービス</li>
-            <li>ソフィア英数セミナー</li>
-            <li>株式会社ロフトシステムズ</li>
-            <li>カレッジ・オンライン</li>
-            <li>株式会社カレッジヤード</li>
-            <li>鶴ヶ峰自動車学校</li>
-            <li>LocaliST株式会社</li>
+            <li>株式会社ねぎしフードサービス</li>
+            <li>セイコーモータースクール</li>
+            <li>マクセル株式会社</li>
+            <li>横浜にぎわい座</li>
+            <li>みたけ食品工業株式会社</li>
+            <li>UNI COFFEE ROASTERY</li>
+            <li>エスエスケイフーズ株式会社</li>
+            <li>横浜ロイヤルパークホテル</li>
+            <li>三菱みなとみらい技術館</li>
+            <li>鎌倉まめや</li>
+            <li>株式会社タスカジ</li>
+            <li>横浜F・マリノス株式会社</li>
+            <li>はまぎん こども美術館</li>
+            <li>そごう美術館</li>
+            <li>体感型動物園iZoo</li>
+            <li>株式会社セザンヌ化粧品</li>
+            <li>カツウラ化粧品株式会社</li>
+            <li>郵政博物館</li>
+            <li>株式会社マンナンライフ</li>
+            <li>ケンミン食品株式会社</li>
+            <li>kino cinema</li>
+            <li>株式会社ザ・キッス</li>
+            <li>株式会社 伊豆シャボテン公園</li>
+            <li>株式会社マザー牧場</li>
+            <li>箱根ラリック美術館</li>
+            <li>スカイスパYOKOHAMA</li>
+            <li>アートプリントジャパン</li>
           </ul>
         </div>
       </section>
       <div style="width:min(100% - 1rem, 27rem)">
-        <section class="section fadeLeft delay06" style="margin-bottom: 20px">
+        <section class="section fadeLeft delay06">
+          <div class="section_head"/>
+          <div class="section_content">
+            <h2>パンフレット協賛</h2>
+            <hr>
+            <ul>
+              <li>株式会社有隣堂</li>
+              <li>横浜弁当</li>
+              <li>寺田倉庫株式会社</li>
+              <li>ユニゾン英語学院</li>
+              <li>杉本歯科医院</li>
+              <li>ねぎしフードサービス</li>
+              <li>ソフィア英数セミナー</li>
+              <li>株式会社ロフトシステムズ</li>
+              <li>カレッジ・オンライン</li>
+              <li>株式会社カレッジヤード</li>
+              <li>鶴ヶ峰自動車学校</li>
+            </ul>
+          </div>
+        </section>
+        <section class="section fadeLeft delay08">
           <div class="section_head"/>
           <div class="section_content">
             <h2>バナー協賛</h2>
@@ -34,13 +72,12 @@
               <li>横浜モアーズ</li>
               <li>株式会社中萬学院</li>
               <li>Uni coffee roastery</li>
-              <li>LocaliST株式会社①</li>
-              <li>LocaliST株式会社②</li>
+              <li>LocaliST株式会社</li>
               <li>株式会社アットヨコハマ</li>
             </ul>
           </div>
         </section>
-        <section class="section fadeLeft delay08">
+        <section class="section fadeLeft delay10">
           <div class="section_head"/>
           <div class="section_content">
             <h2>寄付</h2>
@@ -61,25 +98,35 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.title {
-  box-sizing: border-box;
-  padding: 2rem 0 1rem;
-  text-align: center;
-  margin-block: 0;
-  margin: 0;
-  width: 100%;
-  font-size: 3rem;
+#page_top {
   background: linear-gradient(120deg, #ff5eccdd, #ff75a8dd);
-  color: white;
+  padding: 2rem 2rem 1rem;
+
+  .title {
+    box-sizing: border-box;
+    text-align: center;
+    margin-block: 0;
+    margin: 0;
+    width: 100%;
+    font-size: 3rem;
+    color: white;
+  }
+
+  > div {
+    text-indent: 2rem;
+    margin: 1rem 0;
+    width: unquote("min(100% - 2rem, 40rem)");
+    font-size: 1.4rem;
+    text-align: left;
+    margin-inline: auto;
+  }
 }
 
 .section_holder {
-  margin-top: 20px;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   flex-wrap: wrap;
-  gap: 1rem;
-  padding-bottom: 3rem;
+  padding: 1rem 10px;  gap: 0 10px;
 }
 
 .section {
@@ -90,6 +137,7 @@ export default {
   border-radius: 0.5rem;
   overflow: hidden;
   z-index: 10;
+  margin-bottom: 1rem;
   background: #fffe;
 
   .section_head {
@@ -119,6 +167,7 @@ export default {
 
     li {
       font-size: 1.5rem;
+      list-style: none;
     }
   }
 }
@@ -129,7 +178,7 @@ export default {
   color: white;
   text-align: center;
   box-sizing: border-box;
-  width: unquote("min(100% - 2rem, 70rem)");
+  width: unquote("min(100% - 1rem, 70rem)");
   margin-inline: auto;
   position: relative;
   overflow: clip;
@@ -152,32 +201,34 @@ export default {
   justify-content: center;
   align-items: center;
   gap: 1rem 2rem;
+
   div {
     flex-basis: calc(50% - 2rem);
   }
 }
 
-.delay04{
+.delay04 {
   animation-delay: 0.4s;
 }
 
-.delay06{
+.delay06 {
   animation-delay: 0.6s;
 }
-.delay08{
+
+.delay08 {
   animation-delay: 0.8s;
 }
 
+.delay10 {
+  animation-delay: 1.0s;
+}
 
 
-@media screen and (max-width: 600px) {
+
+@media screen and (max-width: 1000px) {
   .content-frame {
     .title {
       font-size: 30px;
-    }
-
-    .section_title {
-      font-size: 25px;
     }
 
     .section_head {
@@ -191,7 +242,63 @@ export default {
 
       li {
         font-size: 23px;
-        list-style: none;
+      }
+
+      ul {
+        padding-left: 1rem;
+      }
+    }
+  }
+  .section_holder {
+    padding-bottom: 1rem;
+  }
+}
+@media screen and (max-width: 600px) {
+  .content-frame {
+    .title {
+      font-size: 30px;
+    }
+
+    .section_head {
+      margin-right: 10px;
+    }
+
+    .section_content {
+      h2 {
+        font-size: 30px;
+      }
+
+      li {
+        font-size: 23px;
+      }
+
+      ul {
+        padding-left: 1rem;
+      }
+    }
+  }
+  .section_holder {
+    padding-bottom: 1rem;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  .content-frame {
+    .title {
+      font-size: 25px;
+    }
+
+    .section_head {
+      margin-right: 8px;
+    }
+
+    .section_content {
+      h2 {
+        font-size: 25px;
+      }
+
+      li {
+        font-size: 18px;
       }
 
       ul {
