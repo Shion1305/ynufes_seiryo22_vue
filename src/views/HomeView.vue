a3f792c40b571ec8611b627c73dfc58bdb2837f6
Shion Ichikawa <shion1305@gmail.com>
2022-04-28 01:02:29 +0900

    [UPDATE] update data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -654,7 +654,7 @@ export default {
     this.scrollTop();
   },
   setup() {
-    useMeta({title: '',description:'横浜国立大学オンライン大学祭「22清陵祭」公式HPです。今年のテーマは『花笑み』!! 様々なオンライン企画をお楽しみください!'})
+    useMeta({title: '',description:'横浜国立大学オンライン大学祭「22清陵祭」公式HPです。今年のテーマは『花笑み』! 楽しいオンライン企画が満載！'})
   }
 }
 </script>
