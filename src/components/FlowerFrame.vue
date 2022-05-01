b0ee6f9df8b76146227f39278ed7843582e68d63
Shion Ichikawa <shion1305@gmail.com>
2022-05-01 22:03:26 +0900

    [IMPLEMENT] FlowerFrame.vue, initial version of AboutView.vue

---
+++ src/components/FlowerFrame.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="flower-area">
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+    <div class="petal"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlowerFrame"
+}
+</script>
+
+<style scoped>
+.flower-area {
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(90deg, #ff7cd3, #ff6498);
+}
+
+.petal {
+  top: -10%;
+  width: 3%;
+  aspect-ratio: 1;
+  border-radius: 100% 0 100% 0;
+  position: relative;
+  background-color: white;
+}
+
+.petal::after {
+  content: "";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-radius: 100% 0 100% 0;
+  transform: rotate(17deg);
+  transform-origin: bottom left;
+}
+
+
+.petal:nth-child(2n)::after{
+  transform-origin: top right;
+  transform: rotate(20deg);
+}
+
+
+.flower-area .petal:nth-child(1) {
+  transform: rotate(180deg);
+  left: 5%;
+  animation: 7s linear .5s infinite running gravity,
+  2.9s ease-in-out infinite alternate swing;
+  opacity: 60%;
+}
+
+.flower-area .petal:nth-child(2) {
+  left: 16%;
+  animation: 7s linear .5s infinite running gravity,
+  3.1s ease-in-out infinite alternate swing1;
+  opacity: 50%;
+}
+
+.flower-area .petal:nth-child(3) {
+  left: 24%;
+  animation: 6s linear .5s infinite running gravity,
+  3.5s ease-in-out infinite alternate swing2;
+  opacity: 60%;
+}
+
+.flower-area .petal:nth-child(4) {
+  left: 38%;
+  animation: 7s linear 4s infinite running gravity,
+  3.2s ease-in-out infinite alternate swing;
+  opacity: 60%;
+}
+
+.flower-area .petal:nth-child(5) {
+  left: 54%;
+  animation: 8s linear 1s infinite running gravity,
+  2.5s ease-in-out infinite alternate swing1;
+  opacity: 60%;
+}
+
+.flower-area .petal:nth-child(6) {
+  left: 67%;
+  animation: 5s linear 3s infinite running gravity,
+  3s ease-in-out infinite alternate swing2;
+  opacity: 60%;
+}
+
+.flower-area .petal:nth-child(7) {
+  left: 89%;
+  animation: 8s linear 5s infinite running gravity,
+  4s ease-in-out infinite alternate swing;
+  opacity: 60%;
+}
+
+
+
+.flower-area {
+  position: relative;
+}
+
+.flower-area .petal {
+  position: absolute;
+}
+
+
+@keyframes swing {
+  from {
+    transform-origin: 50% -300%;
+    transform: rotate(-30deg);
+  }
+  to {
+    transform-origin: 50% -400%;
+    transform: rotate(45deg);
+  }
+}
+
+@keyframes swing1 {
+  from {
+    transform-origin: 50% -600%;
+    transform: rotate(-10deg);
+  }
+  to {
+    transform-origin: 50% -800%;
+    transform: rotate(55deg);
+  }
+}
+
+@keyframes swing2 {
+  from {
+    transform-origin: 50% -1000%;
+    transform: rotate(-30deg);
+  }
+  to {
+    transform-origin: 50% -1200%;
+    transform: rotate(30deg);
+  }
+}
+
+
+@keyframes gravity {
+  from {
+    top: -10%;
+  }
+  to {
+    top: 120%;
+  }
+}
+
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,183 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-head">
+    <div class="about-head-text">
+      <div>
+        <h1>22清陵祭</h1>
+        <h2>『花笑み』</h2>
+      </div>
+      <div class="flower-background">
+        <FlowerFrame/>
+      </div>
+    </div>
+    <img src="/image/twitter_ogp.jpg"/>
+  </div>
+  <div class="about-content">
+
+  </div>
+
+
+  <div>
+    <ul class="departments-structure">
+      <li>
+        <ul>
+          <li>機械・材料・海洋系学科
+            <ul>
+              <li>機械工学EP</li>
+              <li>材料工学EP</li>
+              <li>海洋空間のシステムデザインEP</li>
+            </ul>
+          </li>
+          <li>化学・生命系学科
+            <ul>
+              <li>化学EP</li>
+              <li>化学応用EP</li>
+              <li>バイオEP</li>
+            </ul>
+          </li>
+          <li>
+            数物・電子情報系学科
+            <ul>
+              <li>
+                数理科学EP
+              </li>
+              <li>
+                物理工学EP
+              </li>
+              <li>
+                電子情報システムEP
+              </li>
+              <li>
+                情報工学EP
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div>理工学部</div>
+      </li>
+    </ul>
   </div>
 </template>
+<style lang="scss" scoped>
+//:root {
+//  display: flex;
+//  flex-direction: column;
+//  justify-content: center;
+//}
+
+.departments-structure {
+  list-style: none;
+
+  ul {
+    list-style: none;
+  }
+}
+
+
+.departments-structure {
+  box-sizing: border-box;
+
+  > li {
+    box-sizing: border-box;
+  }
+
+  > li > div {
+    height: 100%;
+    position: absolute;
+    transform: translateY(-50%);
+  }
+}
+
+.about-head {
+  border-radius: 2.5rem;
+  overflow: hidden;
+  color: white;
+  box-sizing: border-box;
+  width: unquote("min(100% - 6rem, 50rem)");
+  display: flex;
+  flex-direction: row;
+  z-index: 10;
+  margin: 0;
+  box-shadow:
+      0 1.9px 2.5px rgba(0, 0, 0, 0.057),
+      0 5px 6.1px rgba(0, 0, 0, 0.076),
+      0 10.1px 11.4px rgba(0, 0, 0, 0.086),
+      0 19.2px 19.8px rgba(0, 0, 0, 0.092),
+      0 38.4px 34.8px rgba(0, 0, 0, 0.1),
+      0 101px 74px rgba(0, 0, 0, 0.13);
+  .about-head-text {
+    position: relative;
+    font-family: 'Kaisei Decol', serif;
+    flex-basis: 50%;
+    flex-shrink: 0;
+    aspect-ratio: 1;
+
+    div {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+    }
+
+    h1 {
+      font-size: 4.5rem;
+      margin: 0;
+      font-weight: normal;
+    }
+
+    h2 {
+      font-weight: normal;
+      text-align: right;
+      font-size: 3.5rem;
+      margin: 1rem 0 0 0;
+    }
+
+  }
+
+  img {
+    width: 100%;
+    aspect-ratio: 1;
+    height: auto;
+    object-fit: cover;
+    overflow: hidden;
+  }
+
+  .flower-background {
+    aspect-ratio: 1;
+    z-index: -10;
+    margin: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+  }
+}
+
+.about-content{
+  border-radius: 5rem;
+  transform: translateY(-100px);
+  width: unquote("min(100% - 2rem, 70rem)");
+  height: 300px;
+  background: white;
+
+  box-shadow:
+      0 1.9px 2.5px rgba(0, 0, 0, 0.057),
+      0 5px 6.1px rgba(0, 0, 0, 0.076),
+      0 10.1px 11.4px rgba(0, 0, 0, 0.086),
+      0 19.2px 19.8px rgba(0, 0, 0, 0.092),
+      0 38.4px 34.8px rgba(0, 0, 0, 0.1),
+      0 101px 74px rgba(0, 0, 0, 0.13);
+}
+</style>
+
+
 <script>
 import {useMeta} from "vue-meta";
+import FlowerFrame from "@/components/FlowerFrame";
 
 export default {
+  components: {FlowerFrame},
   setup() {
-    useMeta({title: '清陵祭とは',description:"「22清陵祭」を開催するにあたりご協賛くださった企業の一覧を掲載しています。数多くの企業の皆様に多大なご協力を賜りました。心より感謝申し上げます。"})
+    useMeta({title: '清陵祭とは', description: "「22清陵祭」を開催するにあたりご協賛くださった企業の一覧を掲載しています。数多くの企業の皆様に多大なご協力を賜りました。心より感謝申し上げます。"})
   }
 }
 </script>
