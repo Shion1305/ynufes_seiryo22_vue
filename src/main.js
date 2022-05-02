24639e5fe6e0171f0f8dff98d1c5007668ed0ba3
Shion Ichikawa <shion1305@gmail.com>
2022-05-03 04:57:20 +0900

    [IMPLEMENT] implement Google Analytics (gtag), working for #2

--- src/main.js
+++ src/main.js
@@ -4,13 +4,28 @@ import router from './router'
 import store from './store'
 import {createMetaManager} from 'vue-meta'
 // import TwitterFeed from "vuejs-twitter-feed";
-
+import VueGtag from "vue-gtag";
 
 /**
  * refer https://stackoverflow.com/questions/66228340/how-to-use-vue-3-meta-with-vue-js-3 for vue-meta(for Vue3, alpha version)
  */
 
-createApp(App).use(store)
-    .use(createMetaManager())
-    // .use(TwitterFeed)
-    .use(router).mount('#app')
+
+if (process.env.NODE_ENV === "production") {
+    createApp(App).use(store)
+        .use(createMetaManager())
+        // .use(TwitterFeed)
+        .use(router)
+        .mount('#app')
+} else {
+    createApp(App).use(store)
+        .use(createMetaManager())
+        // .use(TwitterFeed)
+        .use(router)
+        .use(
+            VueGtag,
+            {
+                config: {id: "G-88XSGWF51S"},
+            }
+        ).mount('#app')
+}
